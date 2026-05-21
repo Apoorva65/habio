f9e006485501aeee8b81b776ac407989c27a562e
Apoorva65 <apoorvasdixit@gmail.com>
2026-05-21 13:45:09 +0530

    Set up frontend

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import habitRoutes from './routes/habitRoutes.js'
 import completedRoutes from './routes/completedRoutes.js'
+import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT || 5001
 
 app.use(express.json());
+app.use(cors())
 
 app.use('/api/habits',habitRoutes)
 app.use('/api/completed',completedRoutes)
